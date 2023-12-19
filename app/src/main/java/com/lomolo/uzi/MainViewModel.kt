8053b899bb43e50275dc74b8b43ca2db5e9078b2
3dw1nM0535 <workockmoses@gmail.com>
2023-12-19 20:27:18 +0300

    chore: ip info updates

--- app/src/main/java/com/lomolo/uzi/MainViewModel.kt
+++ app/src/main/java/com/lomolo/uzi/MainViewModel.kt
@@ -1,10 +1,12 @@
 package com.lomolo.uzi
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.LatLng
 import com.lomolo.uzi.network.UziRestApiService
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -16,27 +18,37 @@ class MainViewModel(
     private val _deviceDetails: MutableStateFlow<DeviceDetails> = MutableStateFlow(DeviceDetails())
     val deviceDetailsUiState = _deviceDetails.asStateFlow()
 
+    var deviceDetailsState: DeviceDetailsUiState by mutableStateOf(DeviceDetailsUiState.Loading)
+        private set
+
     fun setDeviceLocation(gps: LatLng) {
         _deviceDetails.update {
             it.copy(gps = gps)
         }
     }
 
-    init {
-        viewModelScope.launch(Dispatchers.IO) {
+    private fun getIpinfo() {
+        viewModelScope.launch {
+            deviceDetailsState = DeviceDetailsUiState.Loading
             try {
                 val response = uziRestApiService.getIpinfo()
                 val ipGps = response.location.split(",")
                 _deviceDetails.update {
+                    deviceDetailsState = DeviceDetailsUiState.Success
                     it.copy(
                         gps = LatLng(ipGps[0].toDouble(), ipGps[1].toDouble()),
                         country = response.country
                     )
                 }
             } catch (e: Throwable) {
+                deviceDetailsState = DeviceDetailsUiState.Error(e.localizedMessage)
                 e.printStackTrace()
             }
         }
+    }
+
+    init {
+       getIpinfo()
     }
 }
 
@@ -44,3 +56,9 @@ data class DeviceDetails(
     val gps: LatLng = LatLng(0.0, 0.0),
     val country: String = ""
 )
+
+interface DeviceDetailsUiState {
+    data object Loading: DeviceDetailsUiState
+    data object Success: DeviceDetailsUiState
+    data class Error(val message: String?): DeviceDetailsUiState
+}
--- app/src/main/java/com/lomolo/uzi/compose/home/HomeScreen.kt
+++ app/src/main/java/com/lomolo/uzi/compose/home/HomeScreen.kt
@@ -2,6 +2,8 @@ package com.lomolo.uzi.compose.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -9,6 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.maps.android.compose.GoogleMap
@@ -16,6 +19,7 @@ import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.lomolo.uzi.DeviceDetailsUiState
 import com.lomolo.uzi.MainViewModel
 import com.lomolo.uzi.compose.loader.Loader
 import com.lomolo.uzi.compose.navigation.Navigation
@@ -31,37 +35,41 @@ fun HomeScreen(
     mainViewModel: MainViewModel = viewModel(),
 ) {
     val deviceDetails by mainViewModel.deviceDetailsUiState.collectAsState()
-    val isValidGps = {
-        val gps = deviceDetails.gps
-        gps.latitude != 0.0 && gps.longitude != 0.0
-    }
 
     Box(modifier.fillMaxSize()) {
-        if (isValidGps()) {
-            val uiSettings by remember {
-                mutableStateOf(MapUiSettings(zoomControlsEnabled = false))
-            }
-            val mapProperties by remember {
-                mutableStateOf(MapProperties(mapType = MapType.TERRAIN))
-            }
-            val cameraPositionState = rememberCameraPositionState {
-                position = CameraPosition.fromLatLngZoom(
-                    deviceDetails.gps,
-                    16f
+        when(val s = mainViewModel.deviceDetailsState) {
+            is DeviceDetailsUiState.Loading -> Loader(
+                modifier = Modifier.matchParentSize()
+            )
+            is DeviceDetailsUiState.Error -> {
+                Text(
+                    text = "Something went wrong and it's not your fault",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier.align(Alignment.Center)
                 )
             }
+            is DeviceDetailsUiState.Success -> {
+                val uiSettings by remember {
+                    mutableStateOf(MapUiSettings(zoomControlsEnabled = false))
+                }
+                val mapProperties by remember {
+                    mutableStateOf(MapProperties(mapType = MapType.TERRAIN))
+                }
+                val cameraPositionState = rememberCameraPositionState {
+                    position = CameraPosition.fromLatLngZoom(
+                        deviceDetails.gps,
+                        16f
+                    )
+                }
 
-            GoogleMap(
-                modifier = Modifier.matchParentSize(),
-                properties = mapProperties,
-                uiSettings = uiSettings,
-                cameraPositionState = cameraPositionState
-            )
-        } else {
-            Loader(
-                modifier = Modifier
-                    .align(Alignment.Center)
-            )
+                GoogleMap(
+                    modifier = Modifier.matchParentSize(),
+                    properties = mapProperties,
+                    uiSettings = uiSettings,
+                    cameraPositionState = cameraPositionState
+                )
+            }
         }
     }
 }
