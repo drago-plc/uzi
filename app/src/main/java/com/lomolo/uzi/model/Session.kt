0b61d38f52cd7d9a0a854f0a9cba7906f764064f
3dw1nM0535 <workockmoses@gmail.com>
2023-12-30 23:02:24 +0300

    chore: cleanup model

--- app/src/main/java/com/lomolo/uzi/model/Session.kt
+++ app/src/main/java/com/lomolo/uzi/model/Session.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "sessions")
 data class Session(
-    @PrimaryKey(autoGenerate = false) val id: String = "",
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val token: String = "",
     val courierStatus: CourierStatus = CourierStatus.OFFLINE,
     val isCourier: Boolean = false,
