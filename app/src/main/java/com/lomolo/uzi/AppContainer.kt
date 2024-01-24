273fe03658947426e159d3c29ff211986cafde4b
3dw1nM0535 <workockmoses@gmail.com>
2024-01-24 12:09:54 +0300

    chore: api endpoint

--- app/src/main/java/com/lomolo/uzi/AppContainer.kt
+++ app/src/main/java/com/lomolo/uzi/AppContainer.kt
@@ -30,6 +30,8 @@ interface AppContainer {
     val apolloClient: ApolloClient
 }
 
+private const val baseApi = "https://795c-102-217-124-1.ngrok-free.app"
+
 class AuthInterceptor(
     private val sessionDao: SessionDao,
     private val sessionRepository: SessionInterface
@@ -80,7 +82,7 @@ class DefaultContainer(private val context: Context): AppContainer {
         .build()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("https://8480-102-217-127-1.ngrok-free.app")
+        .baseUrl(baseApi)
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .client(okhttpClient)
         .build()
@@ -97,7 +99,7 @@ class DefaultContainer(private val context: Context): AppContainer {
     }
 
     override val apolloClient = ApolloClient.Builder()
-        .serverUrl("https://8480-102-217-127-1.ngrok-free.app/api")
+        .serverUrl("${baseApi}/api")
         .addHttpInterceptor(
             AuthInterceptor(
                 UziStore.getStore(context).sessionDao(),
